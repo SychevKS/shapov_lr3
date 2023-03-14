7e28f3e5b813550852eedafea649f647524d9182
kirill sychev <kirill>
2023-03-14 17:01:14 +0500

    готово

--- shapov_lr3/shapov_lr3.cpp
+++ shapov_lr3/shapov_lr3.cpp
@@ -1,6 +1,75 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <chrono>
+
+std::vector<std::vector<int>> sequential_multiply_matrices(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B) {
+    int n = A.size();
+    int m = B.size();
+    int k = B[0].size();
+
+    std::vector<std::vector<int>> C(n, std::vector<int>(k));
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < k; ++j) {
+            for (int t = 0; t < m; ++t) {
+                C[i][j] += A[i][t] * B[t][j];
+            }
+        }
+    }
+    return C;
+}
+
+std::vector<std::vector<int>> parallel_multiply_matrices(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B) {
+    int n = A.size();
+    int m = B.size();
+    int k = B[0].size();
+
+    std::vector<std::vector<int>> C(n, std::vector<int>(k));
+
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < k; ++j) {
+            for (int t = 0; t < m; ++t) {
+                C[i][j] += A[i][t] * B[t][j];
+            }
+        }
+    }
+
+    return C;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int sizeMtx = 1000;
+    srand(time(0));
+
+    std::vector<std::vector<int>> A(sizeMtx, std::vector<int>(sizeMtx));
+    std::vector<std::vector<int>> B(sizeMtx, std::vector<int>(sizeMtx));
+
+    for (int i = 0; i < sizeMtx; ++i) {
+        for (int j = 0; j < sizeMtx; ++j) {
+            A[i][j] = rand() % 100 + 1;
+        }
+    }
+    for (int i = 0; i < sizeMtx; ++i) {
+        for (int j = 0; j < sizeMtx; ++j) {
+            B[i][j] = rand() % 100 + 1;
+        }
+    }
+
+    // Последовательное умножение
+    auto start = std::chrono::steady_clock::now();
+    std::vector<std::vector<int>> seq_mtx = sequential_multiply_matrices(A, B);
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds_par = end - start;
+    std::cout << "Sequential multiplication took " << elapsed_seconds_par.count() << " seconds." << std::endl;
+
+    //Параллельное умножение
+    auto start1 = std::chrono::steady_clock::now();
+    std::vector<std::vector<int>> par_mtx = parallel_multiply_matrices(A, B);
+    auto end1 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds_par1 = end1 - start1;
+    std::cout << "Parallel multiplication took " << elapsed_seconds_par1.count() << " seconds." << std::endl;
 }
