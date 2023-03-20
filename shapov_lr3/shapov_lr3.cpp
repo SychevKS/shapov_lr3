ef9d9af52d324bc6521717b4eed9ed342c852b8d
79965 <79965@DESKTOP-6GK2793>
2023-03-20 16:57:29 +0500

    включение openMP и добавление оптимизации

--- shapov_lr3/shapov_lr3.cpp
+++ shapov_lr3/shapov_lr3.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <vector>
 #include <chrono>
+#include <omp.h>
 
 std::vector<std::vector<int>> sequential_multiply_matrices(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B) {
     int n = A.size();
@@ -28,7 +29,7 @@ std::vector<std::vector<int>> parallel_multiply_matrices(const std::vector<std::
 
     std::vector<std::vector<int>> C(n, std::vector<int>(k));
 
-    #pragma omp paralell for private (j, t) 
+    #pragma omp parallel for private
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < k; ++j) {
             for (int t = 0; t < m; ++t) {
@@ -40,48 +41,18 @@ std::vector<std::vector<int>> parallel_multiply_matrices(const std::vector<std::
     return C;
 }
 
-std::vector<std::vector<int>> block_multiply_matrices(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B, int block_size) {
+std::vector<std::vector<int>> change_parallel_multiply_matrices(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B) {
     int n = A.size();
     int m = B.size();
     int k = B[0].size();
 
     std::vector<std::vector<int>> C(n, std::vector<int>(k));
 
-    for (int i = 0; i < n; i += block_size) {
-        for (int j = 0; j < k; j += block_size) {
-            for (int t = 0; t < m; t += block_size) {
-                for (int ii = i; ii < std::min(i + block_size, n); ii++) {
-                    for (int jj = j; jj < std::min(j + block_size, k); jj++) {
-                        for (int tt = t; tt < std::min(t + block_size, m); tt++) {
-                            C[ii][jj] += A[ii][tt] * B[tt][jj];
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    return C;
-}
-
-std::vector<std::vector<int>> block_multiply_matrices_parallel(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B, int block_size) {
-    int n = A.size();
-    int m = B.size();
-    int k = B[0].size();
-
-    std::vector<std::vector<int>> C(n, std::vector<int>(k));
-
-    #pragma omp parallel for schedule(static)
-    for (int i = 0; i < n; i += block_size) {
-        for (int j = 0; j < k; j += block_size) {
-            for (int t = 0; t < m; t += block_size) {
-                for (int ii = i; ii < std::min(i + block_size, n); ii++) {
-                    for (int jj = j; jj < std::min(j + block_size, k); jj++) {
-                        for (int tt = t; tt < std::min(t + block_size, m); tt++) {
-                            C[ii][jj] += A[ii][tt] * B[tt][jj];
-                        }
-                    }
-                }
+    #pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+        for (int t = 0; t < m; ++t){
+            for (int j = 0; j < k; ++j) {
+                C[i][j] += A[i][t] * B[t][j];
             }
         }
     }
@@ -91,7 +62,7 @@ std::vector<std::vector<int>> block_multiply_matrices_parallel(const std::vector
 
 int main()
 {
-	int sizeMtx = 500;
+	int sizeMtx = 1000;
     int block_size = 10;
     srand(time(0));
 
@@ -123,17 +94,10 @@ int main()
     std::chrono::duration<double> elapsed_seconds_par1 = end1 - start1;
     std::cout << "Parallel multiplication took " << elapsed_seconds_par1.count() << " seconds." << std::endl;
 
-    // Блоковый алгоритм
+    // Параллельное умножение c инверсией внутренних циклов
     auto start2 = std::chrono::steady_clock::now();
-    std::vector<std::vector<int>> block_mtx = block_multiply_matrices(A, B, block_size);
+    std::vector<std::vector<int>> change_par_mtx = change_parallel_multiply_matrices(A, B);
     auto end2 = std::chrono::steady_clock::now();
     std::chrono::duration<double> elapsed_seconds_par2 = end2 - start2;
-    std::cout << "Block multiplication took " << elapsed_seconds_par2.count() << " seconds." << std::endl;
-
-    //Блоковый алгоритм с OpenMP
-    auto start3 = std::chrono::steady_clock::now();
-    std::vector<std::vector<int>> block_mtx_parallel = block_multiply_matrices_parallel(A, B, block_size);
-    auto end3 = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_seconds_par3 = end3 - start3;
-    std::cout << "Block parallel multiplication took " << elapsed_seconds_par3.count() << " seconds." << std::endl;
+    std::cout << "Cycle swapping parallel multiplication took " << elapsed_seconds_par2.count() << " seconds." << std::endl;
 }
